c012e4d2390bc6c8cb22f4fb0758495e93a6b548
Stephen Rathbone <stephen@stephenrathbone.co.uk>
2019-04-27 19:48:00 +0100

    Improve bash files

--- .config/tmux/mem-usage.sh
+++ .config/tmux/mem-usage.sh
@@ -3,13 +3,13 @@
 . ${0%/*}/colours.sh
 
 main() {
-    used_mem=`free -h | awk '/Mem:/ { print $3 }' | awk '{$1=$1};1'`
-    total_mem=`free -h | awk '/Mem:/ { print $2 }'`
-    percent_mem=`free | awk '/Mem/{printf("%i"), $3/$2*100}'`
+    used_mem=$(free -h | awk '/Mem:/ { print $3 }' | awk '{$1=$1};1')
+    total_mem=$(free -h | awk '/Mem:/ { print $2 }')
+    percent_mem=$(free | awk '/Mem/{printf("%i"), $3/$2*100}')
 
-    if [ ${percent_mem} -lt 30 ]; then
+    if [[ "${percent_mem}" -lt 30 ]]; then
         print_green_string "${used_mem}/${total_mem}"
-    elif [ ${percent_mem} -lt 70 ]; then
+    elif [[ "${percent_mem}" -lt 70 ]]; then
         print_yellow_string "${used_mem}/${total_mem}"
     else
         print_red_string "${used_mem}/${total_mem}"
@@ -17,4 +17,3 @@ main() {
 }
 
 main "$@"
-
