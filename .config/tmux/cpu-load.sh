d4727a7ead997839a99f9d1ce2892ff41d8521e7
Stephen Rathbone <stephenr@cpwonlinesolutions.com>
2020-01-06 14:49:01 +0000

    Change CPU and memory limit colours at higher utilization

--- .config/tmux/cpu-load.sh
+++ .config/tmux/cpu-load.sh
@@ -7,9 +7,9 @@ main() {
     cpu_load="$(awk '{ print $1 }' < /proc/loadavg)"
     cpu_load_percent="$(awk '{printf("%i",$1/$2*100)}' <<<" ${cpu_load} ${total_cpu_cores} ")"
 
-    if [[ "${cpu_load_percent}" -lt 30 ]]; then
+    if [[ "${cpu_load_percent}" -lt 50 ]]; then
         print_green_string "${cpu_load}"
-    elif [[ "${cpu_load_percent}" -lt 70 ]]; then
+    elif [[ "${cpu_load_percent}" -lt 80 ]]; then
         print_yellow_string "${cpu_load}"
     else
         print_red_string "${cpu_load}"
